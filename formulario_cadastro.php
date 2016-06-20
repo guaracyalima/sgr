335f36eb3d308a969a6aff36df4d53a7bd40f700
guaracyalima <guaracyaraujolima@gmail.com>
2016-06-20 17:51:06 -0300

    Updates no css

--- formulario_cadastro.php
+++ formulario_cadastro.php
@@ -19,21 +19,21 @@
 
 				<form name="cadastro" method="POST" action="cadastrar.php">
 					<div class="row colum log-in-form">
-						<h4 class="text-center"> Cadastro de usuario</h4>
-					<label>Nome</label>
-						<input name="nome" type="text" id="nome" placeholder="Ex: Dentusco" value="<?php echo $nome;  ?>"/>
+						<h4 class="text-center" id="cabecalhosMenores"> Cadastro de usuario</h4>
+					<label class="labels">Nome</label>
+						<input class="inputCadastros" name="nome" type="text" id="nome" placeholder="Ex: Dentusco" value="<?php echo $nome;  ?>"/>
 
-					<label>Sobre nome</label>	
-						<input name="sobrenome" type="text" id="sobrenome"  placeholder="Ex: Metalhedlino da Silva" value="<?php echo $sobrenome; ?>" />
+					<label class="labels">Sobre nome</label>	
+						<input class="inputCadastros" name="sobrenome" type="text" id="sobrenome"  placeholder="Ex: Metalhedlino da Silva" value="<?php echo $sobrenome; ?>" />
 
-					<label>Email</label>	
-						<input name="email" type="text" id="email" placeholder="Ex: minindiubrabu@proved.com" value="<?php echo $email; ?>" />
+					<label class="labels">Email</label>	
+						<input class="inputCadastros" name="email" type="text" id="email" placeholder="Ex: minindiubrabu@proved.com" value="<?php echo $email; ?>" />
 
-					<label>Nome de usuario</label>	
-						<input name="usuario" type="text" id="usuario" placeholder="Ex: dentusco" value="<?php echo $usuario; ?>" />
+					<label class="labels">Nome de usuario</label>	
+						<input class="inputCadastros" name="usuario" type="text" id="usuario" placeholder="Ex: dentusco" value="<?php echo $usuario; ?>" />
 
-					<label>Descrição</label>	
-						<textarea name="info" placeholder="Ex: I am ROOT" id="info"><?php echo $info; ?></textarea>
+					<label class="labels">Descrição</label>	
+						<textarea class="inputCadastros" name="info" placeholder="Ex: I am ROOT" id="info"><?php echo $info; ?></textarea>
 
 					<input type="submit" name="submit" value="Cadastrar" class="button expanded"/>
 					</div>
---
+++ upload.php
@@ -0,0 +1,26 @@
+<?php
+ // inclui a conexão
+ include_once('config.php');
+
+ // Abre o Arquvio no Modo r (para leitura)
+ $arquivo = fopen ('file.csv', 'r');
+
+ // Lê o conteúdo do arquivo
+ while(!feof($arquivo))
+ {
+  // Pega os dados da linha
+  $linha = fgets($arquivo, 1024);
+
+  // Divide as Informações das celular para poder salvar
+  $dados = explode(';', $linha);
+
+  // Verifica se o Dados Não é o cabeçalho ou não esta em branco
+  if($dados[0] != 'Nome' && !empty($linha))
+  {
+   mysql_query('INSERT INTO emails (nome, email) VALUES ("'.$dados[0].'", "'.$dados[1].'")');
+  }
+ }
+
+ // Fecha arquivo aberto
+ fclose($arquivo);
+?>
